08518a72c9c83e73f6ebafa2c55281ca49480e8c
Anthony <anthony.rivera3@mail.citytech.cuny.edu>
2019-10-02 10:47:31 -0400

    Uploading Project 1: Control

---
+++ Control/Project1Control/Assets/Scripts/FirstCubeMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstCubeMovement : MonoBehaviour {
+
+    static int HowManyCubesAreLeft = 27;
+
+    public GameObject RubixCube;
+
+
+    Vector3 positionOffset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        positionOffset = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(Mathf.Tan(Time.time) * 3, Mathf.Sin(Time.time) * 7, Mathf.Cos(Time.time) * 3) + positionOffset;
+
+
+        if (HowManyCubesAreLeft == 0)
+        {
+
+            SpawnRubixCube();
+            HowManyCubesAreLeft = 1;
+
+        }
+    }
+
+    void OnMouseDown()
+    {
+
+        Destroy(this.gameObject);
+        Debug.Log("Clicked");
+        HowManyCubesAreLeft = HowManyCubesAreLeft - 1;
+        Debug.Log("There are " + HowManyCubesAreLeft + " cubes left");
+
+    }
+
+    void SpawnRubixCube()
+    {
+
+        Instantiate(RubixCube, new Vector3(0, 2, 0), Quaternion.identity);
+
+    }
+
+
+}
---
+++ Control/Project1Control/Assets/Scripts/RotatingRubixCube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingRubixCube : MonoBehaviour {
+
+    private float spinX = 0.5f;
+    private float spinY = 0.5f;
+    private float spinZ = 0.5f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0, spinY, 0);
+
+    }
+}
